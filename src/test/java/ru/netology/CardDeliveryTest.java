0988f6b3540bba6a4ab8603418271c3888f81383
shalom69 <shalomaevanton@gmail.com>
2023-02-27 23:47:25 +0300

    newdelivery3

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-i
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+
 
 
 import static com.codeborne.selenide.Selenide.$;
@@ -21,21 +19,17 @@ public class CardDeliveryTest {
 
     // 1. Отправка заявки с верно заполненными полями;
     @Test
-    public void shouldReturnSuccessIfFieldsAreFilledInCorrectly() {
-
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
+    public void deliveryCard() {
 
         $("[data-test-id=city] [placeholder='Город']").setValue("Москва");
         $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']");
         $("[data-test-id=name] [type=text]").setValue("Ибрагим Ибрагимов");
         $("[data-test-id=phone] [type=tel]").setValue("+79806314388");
         $("[data-test-id=agreement]").click();
         $("[role=button] .button__content").click();
         $("[data-test-id='notification'] .notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " ));
 
     }
 }
